ee57ae2de4af57e4cf9fefb80a32df6b12241472
Adam <adam.admd@gmail.com>
2020-02-09 20:20:02 +0100

    Debug draw draft fixes

--- source/shared/ccore/internal/debugDraw.c
+++ source/shared/ccore/internal/debugDraw.c
@@ -8,7 +8,7 @@
 #include "furAssert.h"
 
 #define FC_DEBUG_FRAGMENTS_LINES_CAPACITY 4096
-#define FC_DEBUG_VERTEX_SIZE 8
+#define FC_DEBUG_VERTEX_NUM_FLOATS 7
 
 typedef struct fc_debug_fragments_t
 {
@@ -23,7 +23,7 @@ void fc_dbg_init(fc_alloc_callbacks_t* pAllocCallbacks)
 {
 	memset(&g_debugFragments, 0, sizeof(fc_debug_fragments_t));
 	
-	g_debugFragments.linesData = FUR_ALLOC(sizeof(float) * 2 * FC_DEBUG_VERTEX_SIZE * FC_DEBUG_FRAGMENTS_LINES_CAPACITY, 8, FC_MEMORY_SCOPE_DEBUG, pAllocCallbacks);
+	g_debugFragments.linesData = FUR_ALLOC(sizeof(float) * 2 * FC_DEBUG_VERTEX_NUM_FLOATS * FC_DEBUG_FRAGMENTS_LINES_CAPACITY, 8, FC_MEMORY_SCOPE_DEBUG, pAllocCallbacks);
 	g_debugFragments.numLines = 0;
 }
 
@@ -38,12 +38,11 @@ void fc_dbg_line(float start[3], float end[3], float color[4])
 {
 	FUR_ASSERT(g_debugFragments.numLines < FC_DEBUG_FRAGMENTS_LINES_CAPACITY);
 	
-	uint32_t idx = g_debugFragments.numLines * 2 * FC_DEBUG_VERTEX_SIZE;
+	uint32_t idx = g_debugFragments.numLines * 2 * FC_DEBUG_VERTEX_NUM_FLOATS;
 	
 	g_debugFragments.linesData[idx++] = start[0];
 	g_debugFragments.linesData[idx++] = start[1];
 	g_debugFragments.linesData[idx++] = start[2];
-	g_debugFragments.linesData[idx++] = 1.0f;
 	
 	g_debugFragments.linesData[idx++] = color[0];
 	g_debugFragments.linesData[idx++] = color[1];
@@ -53,7 +52,6 @@ void fc_dbg_line(float start[3], float end[3], float color[4])
 	g_debugFragments.linesData[idx++] = end[0];
 	g_debugFragments.linesData[idx++] = end[1];
 	g_debugFragments.linesData[idx++] = end[2];
-	g_debugFragments.linesData[idx++] = 1.0f;
 	
 	g_debugFragments.linesData[idx++] = color[0];
 	g_debugFragments.linesData[idx++] = color[1];
@@ -62,5 +60,5 @@ void fc_dbg_line(float start[3], float end[3], float color[4])
 	
 	g_debugFragments.numLines += 1;
 	
-	FUR_ASSERT(idx == g_debugFragments.numLines * 2 * FC_DEBUG_VERTEX_SIZE);
+	FUR_ASSERT(idx == g_debugFragments.numLines * 2 * FC_DEBUG_VERTEX_NUM_FLOATS);
 }
