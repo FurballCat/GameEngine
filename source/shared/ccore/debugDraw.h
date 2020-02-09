168400d659fb205b8e5ba30eaca0d770557fecd8
Adam <adam.admd@gmail.com>
2020-02-09 18:36:19 +0100

    Draft of debug draw utils.

---
+++ source/shared/ccore/debugDraw.h
@@ -0,0 +1,31 @@
+/* Copyright (c) 2016-2019 Furball Cat */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+	
+#include <inttypes.h>
+#include "api.h"
+	
+typedef struct fc_alloc_callbacks_t fc_alloc_callbacks_t;
+	
+CCORE_API void fc_dbg_init(fc_alloc_callbacks_t* pAllocCallbacks);
+CCORE_API void fc_dbg_release(fc_alloc_callbacks_t* pAllocCallbacks);
+	
+#define FUR_COLOR_WHITE 	{1.0f, 1.0f, 1.0f, 1.0f}
+#define FUR_COLOR_BLACK 	{0.0f, 0.0f, 0.0f, 1.0f}
+#define FUR_COLOR_RED  		{1.0f, 0.0f, 0.0f, 1.0f}
+#define FUR_COLOR_GREEN 	{0.0f, 1.0f, 0.0f, 1.0f}
+#define FUR_COLOR_BLUE 		{0.0f, 0.0f, 1.0f, 1.0f}
+#define FUR_COLOR_CYAN 		{0.0f, 1.0f, 1.0f, 1.0f}
+#define FUR_COLOR_MAGENTA 	{1.0f, 0.0f, 1.0f, 1.0f}
+#define FUR_COLOR_YELLOW 	{1.0f, 1.0f, 0.0f, 1.0f}
+	
+CCORE_API void fc_dbg_line(float start[3], float end[3], float color[4]);
+	
+#ifdef __cplusplus
+}
+#endif // __cplusplus
---
+++ source/shared/ccore/internal/debugDraw.c
@@ -0,0 +1,66 @@
+/* Copyright (c) 2016-2019 Furball Cat */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "debugDraw.h"
+#include "memory.h"
+#include "furAssert.h"
+
+#define FC_DEBUG_FRAGMENTS_LINES_CAPACITY 4096
+#define FC_DEBUG_VERTEX_SIZE 8
+
+typedef struct fc_debug_fragments_t
+{
+	float* linesData;
+	uint32_t numLines;	// todo: this can be atomic, so we will avoid locking the global
+	
+} fc_debug_fragments_t;
+
+fc_debug_fragments_t g_debugFragments;
+
+void fc_dbg_init(fc_alloc_callbacks_t* pAllocCallbacks)
+{
+	memset(&g_debugFragments, 0, sizeof(fc_debug_fragments_t));
+	
+	g_debugFragments.linesData = FUR_ALLOC(sizeof(float) * 2 * FC_DEBUG_VERTEX_SIZE * FC_DEBUG_FRAGMENTS_LINES_CAPACITY, 8, FC_MEMORY_SCOPE_DEBUG, pAllocCallbacks);
+	g_debugFragments.numLines = 0;
+}
+
+void fc_dbg_release(fc_alloc_callbacks_t* pAllocCallbacks)
+{
+	FUR_FREE(g_debugFragments.linesData, pAllocCallbacks);
+	
+	memset(&g_debugFragments, 0, sizeof(fc_debug_fragments_t));
+}
+
+void fc_dbg_line(float start[3], float end[3], float color[4])
+{
+	FUR_ASSERT(g_debugFragments.numLines < FC_DEBUG_FRAGMENTS_LINES_CAPACITY);
+	
+	uint32_t idx = g_debugFragments.numLines * 2 * FC_DEBUG_VERTEX_SIZE;
+	
+	g_debugFragments.linesData[idx++] = start[0];
+	g_debugFragments.linesData[idx++] = start[1];
+	g_debugFragments.linesData[idx++] = start[2];
+	g_debugFragments.linesData[idx++] = 1.0f;
+	
+	g_debugFragments.linesData[idx++] = color[0];
+	g_debugFragments.linesData[idx++] = color[1];
+	g_debugFragments.linesData[idx++] = color[2];
+	g_debugFragments.linesData[idx++] = color[3];
+	
+	g_debugFragments.linesData[idx++] = end[0];
+	g_debugFragments.linesData[idx++] = end[1];
+	g_debugFragments.linesData[idx++] = end[2];
+	g_debugFragments.linesData[idx++] = 1.0f;
+	
+	g_debugFragments.linesData[idx++] = color[0];
+	g_debugFragments.linesData[idx++] = color[1];
+	g_debugFragments.linesData[idx++] = color[2];
+	g_debugFragments.linesData[idx++] = color[3];
+	
+	g_debugFragments.numLines += 1;
+	
+	FUR_ASSERT(idx == g_debugFragments.numLines * 2 * FC_DEBUG_VERTEX_SIZE);
+}
