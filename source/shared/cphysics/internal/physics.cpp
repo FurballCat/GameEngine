67b0d415150ed74df1477fd95b726b5eed6ed1aa
Adam <adam.admd@gmail.com>
2020-08-04 22:55:23 +0200

    Draft Physics init and release functions compiling and linking

--- source/shared/cphysics/internal/physics.cpp
+++ source/shared/cphysics/internal/physics.cpp
@@ -6,3 +6,28 @@
 
 #define NDEBUG
 #include "PxPhysicsAPI.h"
+
+using namespace physx;
+
+struct fp_physics_engine
+{
+	PxFoundation* foundation;
+	PxPhysics* physics;
+};
+
+static PxDefaultAllocator g_defaultAllocator;
+static PxDefaultErrorCallback g_defaultErrorCallback;
+
+fp_physics_engine g_physicsEngine;
+
+void fp_init_physics()
+{
+	g_physicsEngine.foundation = PxCreateFoundation(PX_PHYSICS_VERSION, g_defaultAllocator, g_defaultErrorCallback);
+	g_physicsEngine.physics = PxCreatePhysics(PX_PHYSICS_VERSION, *g_physicsEngine.foundation, PxTolerancesScale());
+}
+
+void fp_release_physics()
+{
+	g_physicsEngine.foundation->release();
+	g_physicsEngine.physics->release();
+}
