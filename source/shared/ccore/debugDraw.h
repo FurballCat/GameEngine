7e7dc6361ec491e8c02db6dc394a35445dd20049
Adam <adam.admd@gmail.com>
2020-12-21 23:13:27 +0100

    Add debug text collecting

--- source/shared/ccore/debugDraw.h
+++ source/shared/ccore/debugDraw.h
@@ -31,6 +31,9 @@ CCORE_API void fc_dbg_triangle(const float pointA[3], const float pointB[3], con
 
 CCORE_API void fc_dbg_plane(const float center[3], const float halfLength, const float color[4]);
 	
+// use sprintf( txt, "blah blah %u", value ) and pass txt to the function (txt is char txt[32] or any other reasonable size)
+CCORE_API void fc_dbg_text(float x, float y, const char* txt, const float color[4]);
+	
 // sizes of each debug fragment buffers
 CCORE_API uint32_t fc_dbg_line_buffer_size(void);
 CCORE_API uint32_t fc_dbg_triangle_buffer_size(void);
@@ -55,6 +58,13 @@ typedef struct fc_dbg_buffers_desc_t
 	
 	const float* trianglesData;
 	uint32_t trianglesDataSize;
+	
+	const float* textLocationData;
+	const uint32_t* textRangeData;
+	uint32_t textLinesCount;
+	
+	const char* textCharactersData;
+	uint32_t textCharacterDataSize;
 } fc_dbg_buffer_desc_t;
 	
 CCORE_API void fc_dbg_get_buffers(fc_dbg_buffer_desc_t* outDesc);
--- source/shared/ccore/internal/debugDraw.c
+++ source/shared/ccore/internal/debugDraw.c
@@ -11,6 +11,12 @@
 #define FC_DEBUG_FRAGMENTS_TRIANGLES_CAPACITY 4096
 #define FC_DEBUG_VERTEX_NUM_FLOATS 7
 
+// 512 lines of 512 characters or 1024 lines of 256 characters or 4096 lines of 64 characters
+#define FC_DEBUG_FRAGMENTS_TEXT_CHARACTERS_CAPACITY 262144
+#define FC_DEBUG_FRAGMENTS_TEXT_LINES_CAPACITY 4096
+#define FC_DEBUG_TEXT_LOCATION_DATA_NUM_FLOATS 5
+#define FC_DEBUG_TEXT_LOCATION_DATA_NUM_RANGE 2
+
 #define FC_DEBUG_LINE_SIZE sizeof(float) * FC_DEBUG_VERTEX_NUM_FLOATS * 2
 #define FC_DEBUG_TRIANGLE_SIZE sizeof(float) * FC_DEBUG_VERTEX_NUM_FLOATS * 3
 
@@ -22,6 +28,12 @@ typedef struct fc_debug_fragments_t
 	float* trianglesData;
 	uint32_t numTriangles;
 	
+	float* textLocationData;	// x, y, r, g, b
+	uint32_t* textRangeData;	// offset, length
+	uint32_t numTextLines;
+	char* textCharactersData;
+	uint32_t numTextCharacters;
+	
 } fc_debug_fragments_t;
 
 fc_debug_fragments_t g_debugFragments;
@@ -30,17 +42,41 @@ void fc_dbg_init(fc_alloc_callbacks_t* pAllocCallbacks)
 {
 	memset(&g_debugFragments, 0, sizeof(fc_debug_fragments_t));
 	
+	// lines alloc
 	g_debugFragments.linesData = FUR_ALLOC(fc_dbg_line_buffer_size(), 8, FC_MEMORY_SCOPE_DEBUG, pAllocCallbacks);
 	g_debugFragments.numLines = 0;
 	
+	// triangles alloc
 	g_debugFragments.trianglesData = FUR_ALLOC(fc_dbg_triangle_buffer_size(), 8, FC_MEMORY_SCOPE_DEBUG, pAllocCallbacks);
 	g_debugFragments.numTriangles = 0;
+	
+	// text alloc
+	{
+		const uint32_t sizeData = sizeof(float) * FC_DEBUG_FRAGMENTS_TEXT_LINES_CAPACITY * FC_DEBUG_TEXT_LOCATION_DATA_NUM_FLOATS;
+		g_debugFragments.textLocationData = FUR_ALLOC(sizeData, 8, FC_MEMORY_SCOPE_DEBUG, pAllocCallbacks);
+	}
+	
+	{
+		const uint32_t sizeData = sizeof(uint32_t) * FC_DEBUG_FRAGMENTS_TEXT_LINES_CAPACITY * FC_DEBUG_TEXT_LOCATION_DATA_NUM_RANGE;
+		g_debugFragments.textRangeData = FUR_ALLOC(sizeData, 8, FC_MEMORY_SCOPE_DEBUG, pAllocCallbacks);
+	}
+	
+	{
+		const uint32_t sizeData = sizeof(char) * FC_DEBUG_FRAGMENTS_TEXT_CHARACTERS_CAPACITY;
+		g_debugFragments.textCharactersData = FUR_ALLOC(sizeData, 8, FC_MEMORY_SCOPE_DEBUG, pAllocCallbacks);
+	}
+	
+	g_debugFragments.numTextCharacters = 0;
+	g_debugFragments.numTextLines = 0;
 }
 
 void fc_dbg_release(fc_alloc_callbacks_t* pAllocCallbacks)
 {
 	FUR_FREE(g_debugFragments.linesData, pAllocCallbacks);
 	FUR_FREE(g_debugFragments.trianglesData, pAllocCallbacks);
+	FUR_FREE(g_debugFragments.textLocationData, pAllocCallbacks);
+	FUR_FREE(g_debugFragments.textRangeData, pAllocCallbacks);
+	FUR_FREE(g_debugFragments.textCharactersData, pAllocCallbacks);
 	
 	memset(&g_debugFragments, 0, sizeof(fc_debug_fragments_t));
 }
@@ -96,6 +132,12 @@ void fc_dbg_get_buffers(fc_dbg_buffer_desc_t* outDesc)
 	
 	outDesc->trianglesData = g_debugFragments.trianglesData;
 	outDesc->trianglesDataSize = fc_dbg_triangles_current_triangles_size();
+	
+	outDesc->textLocationData = g_debugFragments.textLocationData;
+	outDesc->textCharactersData = g_debugFragments.textCharactersData;
+	outDesc->textRangeData = g_debugFragments.textRangeData;
+	outDesc->textLinesCount = g_debugFragments.numTextLines;
+	outDesc->textCharacterDataSize = g_debugFragments.numTextCharacters;
 }
 
 const float* fc_dbg_line_get_data(void)
@@ -137,6 +179,8 @@ void fc_dbg_buffers_clear()
 {
 	g_debugFragments.numLines = 0;
 	g_debugFragments.numTriangles = 0;
+	g_debugFragments.numTextCharacters = 0;
+	g_debugFragments.numTextLines = 0;
 }
 
 void fc_dbg_buffers_unlock(void)
@@ -196,4 +240,33 @@ void fc_dbg_plane(const float center[3], const float halfLength, const float col
 	
 	fc_dbg_triangle(planeC, planeB, planeA, color);
 	fc_dbg_triangle(planeA, planeD, planeC, color);
+}
+
+void fc_dbg_text(float x, float y, const char* txt, const float color[4])
+{
+	const uint32_t length = (uint32_t)strlen(txt);
+	
+	FUR_ASSERT(g_debugFragments.numTextLines < FC_DEBUG_FRAGMENTS_TEXT_LINES_CAPACITY);
+	FUR_ASSERT(g_debugFragments.numTextCharacters + length < FC_DEBUG_FRAGMENTS_TEXT_CHARACTERS_CAPACITY);
+	
+	const uint32_t offsetFloat = g_debugFragments.numTextLines * FC_DEBUG_TEXT_LOCATION_DATA_NUM_FLOATS;
+	const uint32_t offsetRange = g_debugFragments.numTextLines * FC_DEBUG_TEXT_LOCATION_DATA_NUM_RANGE;
+	g_debugFragments.numTextLines += 1;
+
+	float* dataLocation = g_debugFragments.textLocationData + offsetFloat;
+	dataLocation[0] = x;
+	dataLocation[1] = y;
+	dataLocation[2] = color[0];
+	dataLocation[3] = color[1];
+	dataLocation[4] = color[2];
+	
+	const uint32_t offsetCharacters = g_debugFragments.numTextCharacters;
+	g_debugFragments.numTextCharacters += length;
+	
+	char* dataCharacters = g_debugFragments.textCharactersData + offsetCharacters;
+	memcpy(dataCharacters, txt, length);
+	
+	uint32_t* dataRange = g_debugFragments.textRangeData + offsetRange;
+	dataRange[0] = offsetCharacters;
+	dataRange[1] = length;
 }
