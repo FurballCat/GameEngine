bdf7388ecfc8af7056f26b44016b80a38ed783b7
Adam <adam.admd@gmail.com>
2020-12-29 23:38:38 +0100

    Rename FUR_NAME to SID

--- source/shared/ccore/stringHash.h
+++ source/shared/ccore/stringHash.h
@@ -14,7 +14,7 @@ typedef uint32_t fc_string_hash_t;
 
 CCORE_API fc_string_hash_t fc_make_string_hash(const char* name);
 	
-#define FUR_NAME(_name) fc_make_string_hash(_name)
+#define SID(_name) fc_make_string_hash(_name)
 
 #ifdef __cplusplus
 }
