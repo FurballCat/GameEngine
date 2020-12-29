562ad957f42dd7e26a13744b4c9c5255dd439a55
Adam <adam.admd@gmail.com>
2020-12-29 22:19:49 +0100

    Define FUR_NAME

--- source/shared/ccore/stringHash.h
+++ source/shared/ccore/stringHash.h
@@ -13,6 +13,8 @@ extern "C"
 typedef uint32_t fc_string_hash_t;
 
 CCORE_API fc_string_hash_t fc_make_string_hash(const char* name);
+	
+#define FUR_NAME(_name) fc_make_string_hash(_name)
 
 #ifdef __cplusplus
 }
