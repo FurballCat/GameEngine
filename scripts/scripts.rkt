e59740f842c648f3d0f75aa1c4621f4d47e4f727
Adam <adam.admd@gmail.com>
2021-10-31 21:08:12 +0100

    Syntax for defining native c functions in scripts

--- scripts/scripts.rkt
+++ scripts/scripts.rkt
@@ -1,7 +1,5 @@
 #lang racket
 
-(require ecmascript/types)
-
 ;; global compilation flags
 (define use-debug-compilation #f)
 
@@ -18,13 +16,13 @@
   (if use-debug-compilation name result)
   )
 
-;; data writing - result is a list
-(define (variant value)
-  (to-bytes-4 value)
-  )
-
+;; data writing for script buffer
 (define (to-bytes-4 value)
   (integer->integer-bytes value 4 #f)
+  )
+
+(define (variant value)
+  (to-bytes-4 value)
   )
 
 (define (script-op-header op-code flags num-args)
@@ -35,21 +33,42 @@
    )
   )
 
-;; animation functions
-(define (animate object-name anim-name)
-  (list
-   ;; header
-   (script-op-header
-    'animate
-    (if (equal? object-name 'self) 1 0) ;; mark calls for 'self objects for quicker look-up
-    2)
+;; syntax for native c function import: (define-c-function name (arg0 type0) ...)
+(define-syntax define-c-function
+  (syntax-rules ()
+     ;; 1 arg
+     [(_ func-name (arg0 type0))
+     (define (func-name arg0)
+      (list
+       ;; header
+       (script-op-header
+        (quote func-name)
+        (if (equal? arg0 'self) 1 0) ;; mark calls for 'self objects for quicker look-up
+        1)
 
-   ;; args
-   (variant (string-hash object-name))
-   (variant (string-hash anim-name))
-   )
-  )
+       ;; args
+       (variant (type0 arg0))
+       )
+      )]
+     ;; 2 arg
+     [(_ func-name (arg0 type0) (arg1 type1))
+     (define (func-name arg0 arg1)
+      (list
+       ;; header
+       (script-op-header
+        (quote func-name)
+        (if (equal? arg0 'self) 1 0) ;; mark calls for 'self objects for quicker look-up
+        2)
 
+       ;; args
+       (variant (type0 arg0))
+       (variant (type1 arg1))
+       )
+      )]
+    )
+ )
+
+;; simple script buffer saved to single *.fs file
 (define (simple-script name . data)
   (let ((f (open-output-file (string-append (symbol->string name) ".fs") #:mode 'binary #:exists 'replace)))
     (for/list ([e (in-list (flatten data))])
@@ -62,9 +81,13 @@
 ;; =================
 ;; write script here
 
+;; import native c functions
+(define-c-function animate (object-name string-hash) (anim-name string-hash))
+
+;; simple script example
 (simple-script 'idle
                    [animate 'self 'zelda-idle-stand-01]
-                   [animate 'self 'zelda-idle-stand-01]
+                   [animate 'self 'zelda-run-0]
 )
 
 ;; end of the script
